c7f250edd1f6c3b16632150072080537e1c9fbb6
EyadMoneim <Iyad.Abdel-Moneim.2240027@CIT.hnu.edu.eg>
2026-05-21 23:31:09 +0300

    create new comments

--- backend/app/solver.py
+++ backend/app/solver.py
@@ -4,9 +4,8 @@ from app.utils import get_goal_positions
 from app.puzzle import get_neighbors
 from app.heuristics import h_misplaced, h_manhattan, h_euclidean, h_linear_conflict
 
-# دالة مساعدة لاسترجاع المسار في النهاية فقط بدل تخزينه في كل خطوة
-def reconstruct_path(came_from, current_state):
-    path = [list(current_state)]
+def reconstruct_path(came_from, current_state): 
+    path = [list(current_state)] # حول ال tuple ل list عشان اقدر اعرضها في ال frontend
     while current_state in came_from:
         current_state = came_from[current_state]
         path.append(list(current_state))
@@ -14,54 +13,51 @@ def reconstruct_path(came_from, current_state):
     return path
 
 def solve_puzzle(initial_state, size, heuristic_name):
-    initial_tuple = tuple(initial_state)
+    initial_tuple = tuple(initial_state) # علشان اسرع و ينفع احطها في ال set و ال dict
     goal_state = tuple(list(range(1, size * size)) + [0])
     
-    if initial_tuple == goal_state:
+    if initial_tuple == goal_state: #هو أنا كسبان أصلًا ؟ 
         return {"path": [initial_state], "expanded_nodes": 0, "time_taken": 0}
 
-    goal_pos = get_goal_positions(size)
+    goal_pos = get_goal_positions(size)  
     
-    # tie_breaker بيمنع تعارض الـ Priority Queue لو الـ f_score متساوي
     tie_breaker = 0 
     pq = []
     
-    # هندخل (f_score, tie_breaker, g_score, current_state)
     heapq.heappush(pq, (0, tie_breaker, 0, initial_tuple))
     
-    came_from = {} # قاموس لتخزين الأب لكل حالة (بيوفر ميموري رهيبة)
-    visited = set()
+    came_from = {} #الحالة دي جت منين
+    visited = set() #أنا جيت هنا قبل كده
     visited.add(initial_tuple)
     
-    nodes_expanded = 0
-    start_time = time.time()
+    nodes_expanded = 0 #كام حالة فتحناها وفكرنا فيها
+    start_time = time.time() # وقت بداية الحل عشان اقدر احسب الوقت اللي اخدته في الحل
     
-    while pq:
-        f, _, g, current_state = heapq.heappop(pq)
+    while pq: 
+        f, _, g, current_state = heapq.heappop(pq) #هات أقرب طريق للحل
         
-        if current_state == goal_state:
+        if current_state == goal_state: #هل كسبت ؟
             return {
                 "path": reconstruct_path(came_from, current_state),
                 "expanded_nodes": nodes_expanded,
                 "time_taken": round(time.time() - start_time, 4)
-            }
+            } 
             
-        nodes_expanded += 1
+        nodes_expanded += 1 
         
-        # زودنا الوقت لـ 30 ثانية علشان ندي فرصة للأحجام الكبيرة
         if time.time() - start_time > 30:
             return {"error": "Timeout! The puzzle is too scrambled for basic A*. Please try a smaller shuffle."}
 
         for neighbor in get_neighbors(current_state, size):
             if neighbor not in visited:
                 visited.add(neighbor)
-                came_from[neighbor] = current_state # بنسجل إن الجار ده جينا له من الحالة الحالية
-                new_g = g + 1
+                came_from[neighbor] = current_state
+                new_g = g + 1 
                 
                 if heuristic_name == "misplaced":
                     h = h_misplaced(neighbor, size, goal_pos)
                 elif heuristic_name == "manhattan":
-                    h = h_manhattan(neighbor, size, goal_pos)
+                    h = h_manhattan(neighbor, size, goal_pos) #
                 elif heuristic_name == "euclidean":
                     h = h_euclidean(neighbor, size, goal_pos)
                 elif heuristic_name == "linear_conflict":
